dac216406ebb2546d9342fe620030490e35e1ee1
Fabian G <gartlerfabian@gmail.com>
2025-01-13 20:26:04 +0100

    fix8

--- home/user/fdisk/backend/db.js
+++ home/user/fdisk/backend/db.js
@@ -1,13 +1,12 @@
 const mysql = require('mysql2');
-const dotenv = require('dotenv');
 
-dotenv.config();
-
-const db = mysql.createConnection({
+// Create a connection pool
+const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
 });
 
-module.exports = db;
+// Export the promise-based pool
+module.exports = pool.promise();
