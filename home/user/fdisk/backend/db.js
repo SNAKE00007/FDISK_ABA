72bd3ed3be35dd05ccc18cae30c32651cec1cdfe
Fabian G <gartlerfabian@gmail.com>
2025-01-13 22:20:55 +0100

    fix19

--- home/user/fdisk/backend/db.js
+++ home/user/fdisk/backend/db.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 const dotenv = require('dotenv');
 
 dotenv.config();
@@ -14,4 +14,4 @@ const pool = mysql.createPool({
     queueLimit: 0
 });
 
-module.exports = pool.promise();
+module.exports = pool;
