84229d0d251eb5bddcd92b2c793dd75c971b472f
Fabian G <gartlerfabian@gmail.com>
2025-01-13 19:50:51 +0100

    next error

--- home/user/fdisk/backend/controllers/authController.js
+++ home/user/fdisk/backend/controllers/authController.js
@@ -2,13 +2,11 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const { db } = require('../index');
 
-exports.login = (req, res) => {
+exports.login = async (req, res) => {
     const { username, password } = req.body;
 
-    db.query('SELECT * FROM users WHERE username = ?', [username], async (error, results) => {
-        if (error) {
-            return res.status(500).json({ message: 'Server error' });
-        }
+    try {
+        const [results] = await db.promise().query('SELECT * FROM users WHERE username = ?', [username]);
 
         if (results.length === 0) {
             return res.status(401).json({ message: 'Invalid credentials' });
@@ -24,5 +22,7 @@ exports.login = (req, res) => {
         const token = jwt.sign({ id: user.id, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
         res.json({ token, user: { id: user.id, username: user.username, role: user.role } });
-    });
+    } catch (error) {
+        return res.status(500).json({ message: 'Server error' });
+    }
 };
